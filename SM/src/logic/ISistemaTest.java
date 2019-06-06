041a4fcc94d03e56ec7b09340ec8d476b542d310
alumnoFI <alumnoFI@facultades.ort.edu.uy>
2019-06-06 11:02:30 -0300

    Finalizado y probados los tests hasta insertarpalabraydesplazar

--- SM/src/logic/ISistemaTest.java
+++ SM/src/logic/ISistemaTest.java
@@ -13,7 +13,7 @@ public class ISistemaTest {
 
 	@Before
 	public void setUp() throws Exception {
-		sis = new Sistema(4);
+		sis = new Sistema(2);
 	}
 
 	@Test
@@ -25,47 +25,179 @@ public class ISistemaTest {
 
 	@Test
 	public void testDestruirSistemaMensajes() {
-		fail("Not yet implemented");
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.destruirSistemaMensajes();
+		assertEquals(Resultado.OK, ret.resultado);
 	}
 
 	@Test
 	public void testInsertarLinea() {
-		fail("Not yet implemented");
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		assertEquals(Resultado.OK, ret.resultado);
 	}
 
 	@Test
 	public void testInsertarLineaEnPosicion() {
-		fail("Not yet implemented");
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLineaEnPosicion(1);
+		assertEquals(Resultado.OK, ret.resultado);
+	}
+	
+	@Test
+	public void testInsertarLineaEnPosicionMAL1() {
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLineaEnPosicion(486);
+		assertEquals(Resultado.ERROR_1, ret.resultado);
+	}
+	
+	@Test
+	public void testInsertarLineaEnPosicionMAL2() {
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLineaEnPosicion(0);
+		assertEquals(Resultado.ERROR_1, ret.resultado);
 	}
 
 	@Test
 	public void testBorrarLinea() {
-		fail("Not yet implemented");
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.BorrarLinea(1);
+		assertEquals(Resultado.OK, ret.resultado);
 	}
 
 	@Test
+	public void testBorrarLineaMAL1() {
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.BorrarLinea(0);
+		assertEquals(Resultado.ERROR_1, ret.resultado);
+	}
+	
+	@Test
+	public void testBorrarLineaMAL2() {
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.BorrarLinea(6541);
+		assertEquals(Resultado.ERROR_1, ret.resultado);
+	}
+	
+	@Test
 	public void testBorrarTodo() {
-		fail("Not yet implemented");
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.BorrarTodo();
+		assertEquals(Resultado.OK, ret.resultado);
 	}
 
 	@Test
 	public void testBorrarOcurrenciasPalabraEnTexto() {
-		fail("Not yet implemented");
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(1, 1, "PEPE");
+		ret = sis.InsertarPalabraEnLinea(2, 1, "CACA");
+		ret = sis.InsertarPalabraEnLinea(2, 2, "Sape");
+		ret = sis.BorrarOcurrenciasPalabraEnTexto("CACA");
+		
+		assertEquals(Resultado.OK, ret.resultado);
+	}
+	
+	@Test
+	public void testBorrarOcurrenciasPalabraEnTextoMAL1() {
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(1, 1, "PEPE");
+		ret = sis.InsertarPalabraEnLinea(2, 1, "CACA");
+		ret = sis.InsertarPalabraEnLinea(2, 2, "Sape");
+		ret = sis.BorrarOcurrenciasPalabraEnTexto("ASASDASDSAD");
+		
+		assertEquals(Resultado.ERROR_1, ret.resultado);
 	}
 
 	@Test
 	public void testImprimirTexto() {
-		fail("Not yet implemented");
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(1, 1, "Palabra1");
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(2, 1, "Palabra2");
+		ret = sis.InsertarLinea();
+		ret = sis.ImprimirTexto();
+		
+		assertEquals(Resultado.OK, ret.resultado);
+	}
+	
+	@Test
+	public void testImprimirTextoVacio() {
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(1, 1, "Palabra1");
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(2, 1, "Palabra2");
+		ret = sis.InsertarLinea();
+		ret = sis.BorrarTodo();
+		ret = sis.ImprimirTexto();
+		
+		assertEquals(Resultado.OK, ret.resultado);
 	}
 
 	@Test
 	public void testInsertarPalabraEnLinea() {
-		fail("Not yet implemented");
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(1, 1, "Palabra1");
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(1, 2, "Palabra2");
+		
+		assertEquals(Resultado.OK, ret.resultado);
+	}
+	
+	@Test
+	public void testInsertarPalabraEnLineaMAL1() {
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(1, 1, "Palabra1");
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(1, 3, "Palabra2");
+		
+		assertEquals(Resultado.ERROR_2, ret.resultado);
+	}
+	
+	@Test
+	public void testInsertarPalabraEnLineaMAL2() {
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(1, 1, "Palabra1");
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(5, 3, "Palabra2");
+		
+		assertEquals(Resultado.ERROR_1, ret.resultado);
 	}
 
 	@Test
+	public void testInsertarPalabraEnLineaMAL3() {
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(1, 1, "Palabra1");
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraEnLinea(1, 2, "Palabra2");
+		ret = sis.InsertarPalabraEnLinea(1, 3, "Palabra2");
+		
+		assertEquals(Resultado.ERROR_3, ret.resultado);
+	}
+	
+	// Esta hecho hasta ak!
+	@Test
 	public void testInsertarPalabraYDesplazar() {
-		fail("Not yet implemented");
+		Retorno ret = sis.crearSistemaMensajes();
+		ret = sis.InsertarLinea();
+		ret = sis.InsertarPalabraYDesplazar(1, 1, "Palabra1");
+		ret = sis.InsertarPalabraYDesplazar(1, 2, "Palabra3");
+		ret = sis.InsertarPalabraYDesplazar(1, 2, "Palabra2");
+		
+		assertEquals(Resultado.OK, ret.resultado);
 	}
 
 	@Test
--- SM/src/logic/Sistema.java
+++ SM/src/logic/Sistema.java
@@ -1,165 +1,257 @@
 
 package logic;
+
 import listas.*;
 import logic.Retorno.Resultado;
 
 public class Sistema implements ISistema {
-	
-    private int MAX_CANT_PALABRAS_X_LINEA;
-    
-    private Lista<ILista<String>> texto = new Lista<ILista<String>>();
-    
-    public Sistema(int MAX_CANT_PALABRAS_X_LINEA) {
-        this.MAX_CANT_PALABRAS_X_LINEA = MAX_CANT_PALABRAS_X_LINEA;
-    }
 
-    @Override
-    public Retorno crearSistemaMensajes() {
-        this.texto = new Lista<ILista<String>>();
-        return new Retorno(Resultado.OK);
-    }
+	private int MAX_CANT_PALABRAS_X_LINEA;
 
-    @Override
-    public Retorno destruirSistemaMensajes() {
-        for (ILista<String> linea : texto) {
-        	linea.empty();
-        }
-        texto.empty();
-        return new Retorno(Resultado.OK);
-    }
+	private Lista<ILista<String>> texto = new Lista<ILista<String>>(); // Unordered List
+	private Lista<String> diccionario = new Lista<String>(); // Ordered List
 
-    @Override
-    public Retorno InsertarLinea() {
-        texto.addLast(new Lista<String>()); 
-        return new Retorno(Resultado.OK);
-    }
+	public Sistema(int MAX_CANT_PALABRAS_X_LINEA) {
+		this.MAX_CANT_PALABRAS_X_LINEA = MAX_CANT_PALABRAS_X_LINEA;
+	}
 
-    @Override
-    public Retorno InsertarLineaEnPosicion(int posicionLinea) {
-        texto.addAtPosition(new Lista<String>(), posicionLinea);
-        return new Retorno(Resultado.OK);
-    }
+	@Override
+	public Retorno crearSistemaMensajes() {
+		this.texto = new Lista<ILista<String>>();
+		return new Retorno(Resultado.OK);
+	}
 
-    @Override
-    public Retorno BorrarLinea(int posicionLinea) {
-        texto.deleteAtPosition(posicionLinea);
-        return new Retorno(Resultado.OK);
-    }
+	@Override
+	public Retorno destruirSistemaMensajes() {
+		for (ILista<String> linea : texto) {
+			linea.empty();
+		}
+		texto.empty();
+		return new Retorno(Resultado.OK);
+	}
 
-    @Override
-    public Retorno BorrarTodo() {
-        texto.empty();
-        return new Retorno(Resultado.OK);
-    }
+	@Override
+	public Retorno InsertarLinea() {
+		texto.addLast(new Lista<String>());
+		return new Retorno(Resultado.OK);
+	}
 
-    @Override
-    public Retorno BorrarOcurrenciasPalabraEnTexto(String palabraABorrar) {
-    	for (ILista<String> l : texto) {
-    		l.delete(palabraABorrar);
-    	}
-    	return new Retorno(Resultado.OK); 
-    }
+	@Override
+	public Retorno InsertarLineaEnPosicion(int posicionLinea) {
+		
+		if (posicionLinea < 1 || posicionLinea > texto.length()+1) {
+			return new Retorno(Resultado.ERROR_1);
+		}
+		
+		texto.addAtPosition(new Lista<String>(), posicionLinea-1);
+		return new Retorno(Resultado.OK);
+	}
 
-    @Override
-    public Retorno ImprimirTexto() {
-    	int numero_linea = 1;
-        
-    	for (ILista<String> l : texto) {
-        	System.out.print(numero_linea++ + ": ");
-        	l.show();
-        }
-        
-        if (numero_linea == 1) {
-        	System.out.println("Texto vacio");
-        }
-        
-        return new Retorno(Resultado.OK);
-    }
+	@Override
+	public Retorno BorrarLinea(int posicionLinea) {
+		if (posicionLinea < 1 || posicionLinea > texto.length()+1) {
+			return new Retorno(Resultado.ERROR_1);
+		}
+		
+		texto.deleteAtPosition(posicionLinea-1);
+		return new Retorno(Resultado.OK);
+	}
 
-    @Override
-    public Retorno InsertarPalabraEnLinea(int posicionLinea, int posicionPalabra, String palabraAIngresar) {
-        if (posicionLinea < 0 || posicionLinea > texto.length() + 1) {
-        	return new Retorno(Resultado.ERROR_1); // La posicion no es valida
-        }
-    	
-    	ILista<String> linea = texto.getObject(posicionLinea);
-        
-        if (!linea.isFull(MAX_CANT_PALABRAS_X_LINEA)) {
-        
-        	if (posicionPalabra > 0 && posicionPalabra <= linea.length() + 1) {
-        		linea.addAtPosition(palabraAIngresar, posicionPalabra);        		
-        	} else {
-        		return new Retorno(Resultado.ERROR_2); // Pos no valida
-        	}
+	@Override
+	public Retorno BorrarTodo() {
+		texto.empty();
+		return new Retorno(Resultado.OK);
+	}
 
-        } else {
-        	return new Retorno(Resultado.ERROR_3); // Esta llena
-        }
-    	
-        return new Retorno(Resultado.OK);
+	@Override
+	public Retorno BorrarOcurrenciasPalabraEnTexto(String palabraABorrar) {
+		
+		boolean deleted = false;
+		
+		for (ILista<String> l : texto) {
+			if (l.exists(palabraABorrar)) {
+				l.delete(palabraABorrar);
+				deleted = true;
+			}
+		}
+		
+		if (!deleted) {
+			return new Retorno(Resultado.ERROR_1);
+		}
+		
+		return new Retorno(Resultado.OK);
+	}
 
-    }
+	@Override
+	public Retorno ImprimirTexto() {
+		int numero_linea = 1;
+		String text = "";
 
-    @Override
-    public Retorno InsertarPalabraYDesplazar(int posicionLinea, int posicionPalabra, String palabraAIngresar) {
+		for (ILista<String> l : texto) {
+			text += numero_linea++ + ": ";
+			for (String s : l) {
+				text += s;
+			}
+			text += "\n";
+		}
 
-    	if (posicionLinea < 0 || posicionLinea > texto.length() + 1) {
-        	return new Retorno(Resultado.ERROR_1); // La posicion no es valida
-        }
-    	
-    	ILista<String> linea = texto.getObject(posicionLinea);
-    	
-    	if (posicionPalabra < 0 || posicionPalabra > MAX_CANT_PALABRAS_X_LINEA) {
-    		return new Retorno(Resultado.ERROR_2); // La posicion no es valida
-    	}
-    	
-    	linea.addAtPosition(palabraAIngresar, posicionPalabra);
-    	
-    	while(linea.isFull(MAX_CANT_PALABRAS_X_LINEA)) {
-    		String ultima_palabra = linea.getObject(MAX_CANT_PALABRAS_X_LINEA);
-    		linea.deleteAtPosition(MAX_CANT_PALABRAS_X_LINEA);
-    		if (++posicionLinea <= texto.length()) { // ++posicionLinea le suma 1 a la posicionLinea antes de ejecutar el if
-    			linea = texto.getObject(posicionLinea);
-    			linea.addFirst(ultima_palabra);
-    		} else {
-    			texto.addLast(new Lista<String>());
-    			linea = texto.getObject(posicionLinea);
-    			linea.addFirst(ultima_palabra);
-    		}
-    	}
-    	
-    	return new Retorno(Resultado.OK);
-    }
+		if (numero_linea == 1) {
+			return new Retorno(Resultado.OK, 0, "Texto vacío");
+		}
 
-    @Override
-    public Retorno BorrarPalabra(int posicionLinea, int posicionPalabra) {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
+		return new Retorno(Resultado.OK, 0, text);
+	}
 
-    @Override
-    public Retorno ImprimirLinea(int posicionLinea) {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
+	@Override
+	public Retorno InsertarPalabraEnLinea(int posicionLinea, int posicionPalabra, String palabraAIngresar) {
+		if (posicionLinea < 1 || posicionLinea > texto.length() + 1) {
+			return new Retorno(Resultado.ERROR_1); // La posicion no es valida
+		}
 
-    @Override
-    public Retorno IngresarPalabraDiccionario(String palabraAIngresar) {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
+		ILista<String> linea = texto.getObject(posicionLinea-1);
 
-    @Override
-    public Retorno BorrarPalabraDiccionario(String palabraABorrar) {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
+		if (posicionPalabra < 1 || posicionPalabra > linea.length() + 1) {
+			return new Retorno(Resultado.ERROR_2); // Pos no valida
+		}
+		
+		if (linea.isFull(MAX_CANT_PALABRAS_X_LINEA-1)) {
+			return new Retorno(Resultado.ERROR_3); // Esta llena
+		}
+					
+		linea.addAtPosition(palabraAIngresar, posicionPalabra-1);
 
-    @Override
-    public Retorno ImprimirDiccionario() {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
+		return new Retorno(Resultado.OK);
 
-    @Override
-    public Retorno ImprimirTextoIncorrecto() {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
-    
-    
+	}
+
+	@Override
+	public Retorno InsertarPalabraYDesplazar(int posicionLinea, int posicionPalabra, String palabraAIngresar) {
+
+		if (posicionLinea < 1 || posicionLinea > texto.length() + 1) {
+			return new Retorno(Resultado.ERROR_1); // La posicion no es valida
+		}
+
+		ILista<String> linea = texto.getObject(posicionLinea-1);
+
+		if (posicionPalabra < 1 || posicionPalabra > MAX_CANT_PALABRAS_X_LINEA) {
+			return new Retorno(Resultado.ERROR_2); // La posicion no es valida
+		}
+
+		linea.addAtPosition(palabraAIngresar, posicionPalabra-1);
+
+		while (linea.isFull(MAX_CANT_PALABRAS_X_LINEA)) {
+			String ultima_palabra = linea.getObject(MAX_CANT_PALABRAS_X_LINEA);
+			linea.deleteAtPosition(MAX_CANT_PALABRAS_X_LINEA);
+			if (++posicionLinea <= texto.length()) { // ++posicionLinea le suma
+														// 1 a la posicionLinea
+														// antes de ejecutar el
+														// if
+				linea = texto.getObject(posicionLinea-1);
+				linea.addFirst(ultima_palabra);
+			} else {
+				texto.addLast(new Lista<String>());
+				linea = texto.getObject(posicionLinea-1);
+				linea.addFirst(ultima_palabra);
+			}
+		}
+
+		return new Retorno(Resultado.OK);
+	}
+
+	@Override
+	public Retorno BorrarPalabra(int posicionLinea, int posicionPalabra) {
+		if (posicionLinea < 0 || posicionLinea > texto.length() + 1) {
+			return new Retorno(Resultado.ERROR_1); // La posicion no es valida
+		}
+
+		ILista<String> linea = texto.getObject(posicionLinea);
+
+		if (posicionPalabra < 0 || posicionPalabra > linea.length()) {
+			return new Retorno(Resultado.ERROR_2); // La posicion de la palabra
+													// no es valida
+		}
+
+		linea.deleteAtPosition(posicionPalabra);
+
+		return new Retorno(Resultado.OK);
+	}
+
+	@Override
+	public Retorno ImprimirLinea(int posicionLinea) {
+		if (posicionLinea < 0 || posicionLinea > texto.length() + 1) {
+			return new Retorno(Resultado.ERROR_1); // La posicion no es valida
+		}
+
+		ILista<String> linea = texto.getObject(posicionLinea);
+		String texto_linea = "";
+
+		for (String s : linea) {
+			texto_linea += s;
+		}
+
+		return new Retorno(Resultado.OK, 0, texto_linea);
+	}
+
+	@Override
+	public Retorno IngresarPalabraDiccionario(String palabraAIngresar) {
+
+		if (diccionario.exists(palabraAIngresar)) {
+			return new Retorno(Resultado.ERROR_1);
+		}
+
+		diccionario.addInOrder(palabraAIngresar);
+
+		return new Retorno(Resultado.OK);
+	}
+
+	@Override
+	public Retorno BorrarPalabraDiccionario(String palabraABorrar) {
+
+		if (!diccionario.exists(palabraABorrar)) {
+			return new Retorno(Resultado.ERROR_1);
+		}
+
+		diccionario.delete(palabraABorrar);
+
+		return new Retorno(Resultado.OK);
+	}
+
+	@Override
+	public Retorno ImprimirDiccionario() {
+
+		String palabras_diccionario = "";
+
+		for (String p : diccionario) {
+			palabras_diccionario += p + "\n";
+		}
+
+		return new Retorno(Resultado.OK, 0, palabras_diccionario);
+	}
+
+	@Override
+	public Retorno ImprimirTextoIncorrecto() {
+
+		String texto_i = "";
+		
+		int index_line = 1;
+		for (ILista<String> linea_texto : texto) {
+			texto_i += index_line + ": ";
+			for (String p_t : linea_texto) {
+				if (!diccionario.exists(p_t)) {
+					texto_i += p_t + " ";
+				}
+				
+			}
+			texto_i += "\n";
+			index_line++;
+		}
+		
+		
+		if (index_line == 1) {
+			return new Retorno(Resultado.OK, 0, "No hay errores");
+		}
+
+		return new Retorno(Resultado.OK, 0, texto_i);
+	}
+
 }
